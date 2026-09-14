168dd415dd310dac1150c25838da4fc283760d57
HandSte <stefan.hand@gmx.de>
2026-09-14 09:08:12 +0200

    fix: refresh PWA shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'orgaplattform-shell-v1';
-const APP_SHELL = ['/', '/manifest.webmanifest'];
+const CACHE_NAME = 'orgaplattform-shell-v2';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/app-icon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
