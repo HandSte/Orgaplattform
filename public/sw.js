c91d98f4ae4fba91ea6ef91d86b167eba5312f66
HandSte <stefan.hand@gmx.de>
2026-09-15 21:11:54 +0200

    chore: refresh web service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'orgaplattform-shell-v3';
+const CACHE_NAME = 'orgaplattform-shell-v4';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/app-icon.svg'];
 
 self.addEventListener('install', (event) => {
