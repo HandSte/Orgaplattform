7b4f4520b635eb801d7e50642a8992c672e9e5ba
HandSte <stefan.hand@gmx.de>
2026-09-16 18:27:30 +0200

    chore: finalize Essentia internal build config

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -12,10 +12,12 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => ({
     ...(config.ios ?? {}),
     supportsTablet: true,
     bundleIdentifier: 'de.essentia.app',
+    buildNumber: '1',
   },
   android: {
     ...(config.android ?? {}),
     package: 'de.essentia.app',
+    versionCode: 1,
   },
   web: {
     ...(config.web ?? {}),
