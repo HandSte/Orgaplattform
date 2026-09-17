c88ed728e8bb0c30a0cb673ceb4d70b6065d07d2
HandSte <stefan.hand@gmx.de>
2026-09-17 11:19:11 +0200

    style: load todo checklist styles in shared component

--- src/components/BoardCardChecklist.tsx
+++ src/components/BoardCardChecklist.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from 'react';
 import { supabase } from '@/lib/supabase-browser';
+import '@/app/todo-checklist.css';
 
 type ChecklistItem = {
   id: string;
