81c39845dcec8a1df8a8fcd2180a80f1903ee153
HandSte <stefan.hand@gmx.de>
2026-09-18 16:32:10 +0200

    Bump Android release to 0.3.3

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
     ...config,
     name: 'Essentia',
     slug: 'essentia',
-    version: '0.3.2',
+    version: '0.3.3',
     orientation: 'default',
     userInterfaceStyle: 'automatic',
     backgroundColor: '#f1f5f9',
@@ -25,12 +25,12 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
       ...(config.ios ?? {}),
       supportsTablet: true,
       bundleIdentifier: 'de.essentia.app',
-      buildNumber: '5',
+      buildNumber: '6',
     },
     android: {
       ...(config.android ?? {}),
       package: 'de.essentia.app',
-      versionCode: 5,
+      versionCode: 6,
       adaptiveIcon: {
         ...(config.android?.adaptiveIcon ?? {}),
         backgroundColor: '#0f172a',
